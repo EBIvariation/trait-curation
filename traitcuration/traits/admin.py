9fdc5d7d4d710aa190d65a8aef307339e7a39c15
Giorgos Apostolopoulos <s3apostol@gmail.com>
2020-06-04 18:19:42 +0300

    Create data model

---
+++ traitcuration/traits/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from traitcuration.traits.models import Trait, User, OntologyTerm, Mapping, MappingSuggestion, Comment
+# Register your models here.
+
+admin.site.register(Trait)
+admin.site.register(User)
+admin.site.register(Mapping)
+admin.site.register(OntologyTerm)
+admin.site.register(MappingSuggestion)
+admin.site.register(Comment)
---
+++ traitcuration/traits/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+
+
+class Trait(models.Model):
+    name = models.CharField(max_length=200)
+    current_mapping = models.ForeignKey(
+        'Mapping', on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.CharField(max_length=50)
+    number_of_source_records = models.IntegerField(blank=True, null=True)
+    timestamp_added = models.DateTimeField(auto_now=True)
+    timestamp_updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    role = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class OntologyTerm(models.Model):
+    curie = models.CharField(max_length=50, blank=True, null=True)
+    uri = models.URLField(null=True)
+    label = models.CharField(max_length=200, blank=True, null=True)
+    status = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    cross_refs = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.curie} | {self.label}"
+
+
+class Mapping(models.Model):
+    trait_id = models.ForeignKey(Trait, on_delete=models.PROTECT)
+    term_id = models.ForeignKey(OntologyTerm, on_delete=models.PROTECT)
+    curator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    is_reviewed = models.BooleanField()
+    timestamp_mapped = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.trait_id} - {self.term_id}"
+
+
+class MappingSuggestion(models.Model):
+    trait_id = models.ForeignKey(Trait, on_delete=models.PROTECT)
+    term_id = models.ForeignKey(OntologyTerm, on_delete=models.PROTECT)
+    made_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.trait_id} | {self.term_id}"
+
+
+class Comment(models.Model):
+    trait_id = models.ForeignKey(Trait, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+
+    def __str__(self):
+        return f"By {self.author} on {self.date}"
