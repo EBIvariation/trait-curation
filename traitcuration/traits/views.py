34ecd9550c341343e662f62afbca318dcc6f3f18
Giorgos Apostolopoulos <s3apostol@gmail.com>
2020-06-05 13:01:26 +0300

    Create base urls and templates

--- traitcuration/traits/views.py
+++ traitcuration/traits/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def browse(request):
+    return render(request, 'traits/browse.html')
+
+
+def trait_detail(request, pk):
+    return render(request, 'traits/trait_detail.html')
--- traitcuration/urls.py
+++ traitcuration/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('traitcuration.traits.urls')),
+    path('traits/', include('traitcuration.traits.urls')),
+    path('', RedirectView.as_view(url='traits/'))
 ]
