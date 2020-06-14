75fdba0c8375ea2f4916e11e23cb36d5a528919c
Giorgos Apostolopoulos <s3apostol@gmail.com>
2020-06-14 12:47:06 +0300

    Update status_readable_name filter docstring

--- traitcuration/traits/templatetags/trait_filters.py
+++ traitcuration/traits/templatetags/trait_filters.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 
 def status_readable_name(value, count):
-    """Converts the underscore from a status name into a space"""
+    """Converts the underscore from a status name into a space and adds trait count for it"""
     readable_name = value.replace('_', ' ')
     return f"{readable_name} [{count}]"
 
