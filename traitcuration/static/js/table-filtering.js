708e0b8d88974f168d7df0b94da93129e48ade8d
Giorgos Apostolopoulos <s3apostol@gmail.com>
2020-06-15 19:19:18 +0300

    Write documentation and change var to const in table-filtering.js script

--- traitcuration/static/js/table-filtering.js
+++ traitcuration/static/js/table-filtering.js
@@ -1,27 +1,41 @@
-var options = {
+/*
+This script contains all the filtering logic of the main traits table using list.js
+*/
+
+//All the possible values to filter by
+const options = {
   valueNames: ["name", "curie", "label", "status"],
 };
 
-var table = new List("traits-table", options);
+//Initialize the table with the previously created values
+const table = new List("traits-table", options);
 
+//Function to do text filtering. Fires whenever a user types something in the main search box
 function search(searchstring) {
   table.search(searchstring, ["name", "curie", "label"]);
 }
 
+//Function to do filtering by status. Fires whenever a user clicks a status button
 function filterClicked(status, activeClass) {
-  var s = status.getAttribute("data-filter");
-  var filterButtons = document.getElementsByClassName("filter");
-  for (var button of filterButtons) {
+  const s = status.getAttribute("data-filter");
+  //Get all the status buttons and remove their active class if any
+  const filterButtons = document.getElementsByClassName("filter");
+  for (const button of filterButtons) {
     button.classList.remove("button-outlined--primary--active");
     button.classList.remove("button-outlined--success--active");
     button.classList.remove("button-outlined--danger--active");
     button.classList.remove("button-outlined--warning--active");
   }
-  document.getElementById(s).classList.add(`button-outlined--${activeClass}--active`);
+  //Add and active class to the clicked button
+  document
+    .getElementById(s)
+    .classList.add(`button-outlined--${activeClass}--active`);
+  //If the 'all' button is clicked, show all traits
   if (s === "all") {
     table.filter();
     return;
   }
+  //Loop through all the traits and only show the ones matching the selected status
   table.filter(function (item) {
     status = item.elm.getAttribute("status");
     if (status == s) {
