a69eae4b0187b6b106431e695bb590d67cad5ebe
Giorgos Apostolopoulos <s3apostol@gmail.com>
2020-06-15 18:40:54 +0300

    Add review model and make status fields enum

--- traitcuration/traits/admin.py
+++ traitcuration/traits/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from traitcuration.traits.models import Trait, User, OntologyTerm, Mapping, MappingSuggestion, Comment
+from traitcuration.traits.models import Trait, User, OntologyTerm, Mapping, MappingSuggestion, Comment, Review
 # Register your models here.
 
 admin.site.register(Trait)
@@ -8,3 +8,4 @@ admin.site.register(Mapping)
 admin.site.register(OntologyTerm)
 admin.site.register(MappingSuggestion)
 admin.site.register(Comment)
+admin.site.register(Review)
