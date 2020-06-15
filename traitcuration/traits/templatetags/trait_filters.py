a1f763b6ef3596ef3dd6338601caa2c57e50d645
Giorgos Apostolopoulos <s3apostol@gmail.com>
2020-06-15 14:09:36 +0300

    Add labels to status column and links to terms URIs in browse traits table

--- traitcuration/traits/templatetags/trait_filters.py
+++ traitcuration/traits/templatetags/trait_filters.py
@@ -4,7 +4,20 @@ register = template.Library()
 
 
 @register.filter
-def status_readable_name(value, count):
+def get_dict_item(dictionary, key):
+    print(key)
+    attribute_dict = dictionary.get(key)
+    return attribute_dict['class']
+
+
+@register.filter
+def status_readable_name(value):
+    """Converts the underscore from a status name into a space and adds trait count for it"""
+    return value.replace('_', ' ')
+
+
+@register.filter
+def status_readable_name_with_count(value, count):
     """Converts the underscore from a status name into a space and adds trait count for it"""
     readable_name = value.replace('_', ' ')
     return f"{readable_name} [{count}]"
