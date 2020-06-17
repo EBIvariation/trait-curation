fd6cf72c546fb9505a990d779d5342dd185eb7bf
Giorgos Apostolopoulos <s3apostol@gmail.com>
2020-06-17 12:52:27 +0300

    Add top row showing trait info in trait_detail page

--- traitcuration/traits/views.py
+++ traitcuration/traits/views.py
@@ -12,5 +12,6 @@ def browse(request):
 
 def trait_detail(request, pk):
     trait = get_object_or_404(Trait, pk=pk)
-    context = {"trait": trait}
+    status_dict = get_status_dict()
+    context = {"trait": trait, "status_dict": status_dict}
     return render(request, 'traits/trait_detail.html', context)
