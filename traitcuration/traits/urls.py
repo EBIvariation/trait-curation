f9d39bf5cd7cfc38ae77e2be40b6cab62139126a
Giorgos Apostolopoulos <s3apostol@gmail.com>
2020-06-29 13:19:00 +0300

    Add zooma suggestions and ols queries for found terms

--- traitcuration/traits/urls.py
+++ traitcuration/traits/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.browse, name="browse"),
     path('<int:pk>/', views.trait_detail, name="trait_detail"),
     path('datasources/', views.datasources, name="datasources"),
-    path('fetchdata/', views.fetch_data, name="fetch_data")
+    path('clinvar/', views.clinvar_data, name="clinvar_data"),
+    path('zooma/', views.zooma_suggestions, name="zooma_suggestions")
 ]
