adc9bbe0333ee492fc8a454a32c8e8b82e6d1adb
Giorgos Apostolopoulos <s3apostol@gmail.com>
2020-06-30 17:23:05 +0300

    Add dummy data insertion

--- traitcuration/traits/urls.py
+++ traitcuration/traits/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.browse, name="browse"),
     path('<int:pk>/', views.trait_detail, name="trait_detail"),
     path('datasources/', views.datasources, name="datasources"),
-    path('clinvar/', views.clinvar_data, name="clinvar_data"),
-    path('zooma/', views.zooma_suggestions, name="zooma_suggestions")
+    path('datasources/zooma', views.zooma_suggestions, name="zooma_suggestions"),
+    path('datasources/dummy', views.dummy_data, name="dummy_data"),
+    path('datasources/clinvar', views.clinvar_data, name="clinvar_data"),
 ]
