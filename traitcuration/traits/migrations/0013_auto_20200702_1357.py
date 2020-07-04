d5efdac972ecf0bf3e57d0d2d4901c05deb374d5
Giorgos Apostolopoulos <s3apostol@gmail.com>
2020-07-04 13:44:19 +0300

    Add unique together constraint for Mapping model

--- traitcuration/traits/migrations/0013_auto_20200702_1357.py
+++ traitcuration/traits/migrations/0013_auto_20200702_1357.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('traits', '0012_auto_20200702_0924'),
+        ('traits', '0012_auto_20200704_0906'),
     ]
 
     operations = [
