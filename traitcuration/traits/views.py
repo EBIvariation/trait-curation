a163d83e47745216763ea50a80b14eac3a623c19
Giorgos Apostolopoulos <s3apostol@gmail.com>
2020-07-18 18:17:05 +0300

    Hide write operation buttons when signed out

--- traitcuration/traits/views.py
+++ traitcuration/traits/views.py
@@ -23,7 +23,6 @@ def trait_detail(request, pk):
     trait = get_object_or_404(Trait, pk=pk)
     status_dict = get_status_dict()
     context = {"trait": trait, "status_dict": status_dict}
-    print(get_user_info(request))
     return render(request, 'traits/trait_detail.html', context)
 
 
