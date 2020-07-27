b1b59407cd0f7ce041628412d8c5ecbd50d392a1
Giorgos Apostolopoulos <s3apostol@gmail.com>
2020-07-27 17:02:06 +0300

    Create initial automatic mapping functionality

--- traitcuration/traits/datasources/zooma.py
+++ traitcuration/traits/datasources/zooma.py
@@ -5,9 +5,13 @@ terms in the app's database.
 import requests
 import logging
 
+<<<<<<< HEAD
 from django.db import transaction
 
 from ..models import Trait, MappingSuggestion, OntologyTerm, User, Status
+=======
+from ..models import Trait, MappingSuggestion, OntologyTerm, User, Status, Mapping
+>>>>>>> Create initial automatic mapping functionality
 from .ols import make_ols_query, get_ontology_id
 
 logging.basicConfig()
@@ -29,26 +33,62 @@ def run_zooma_for_all_traits():
 
 def run_zooma_for_single_trait(trait):
     logger.info(f"Retrieving ZOOMA suggestions for trait: {trait.name}")
-    suggestion_list = get_zooma_suggestions_for_trait(trait)
-    # A set of suggested terms found in the query, used to exclude those terms from being deleted from the database
-    # when the delete_unused_mappings function is called.
-    suggested_terms = set()
-    for suggestion in suggestion_list:
+
+    datasource_suggestion_list = get_suggestions_from_datasources(trait)
+    # for dsl in datasource_suggestion_list:
+    #     print(dsl["semanticTags"][0])
+    #     print(dsl['confidence'])
+
+    # print('--------------------------------------')
+    ols_suggestion_list = get_suggestions_from_ols(trait)
+    # for osl in ols_suggestion_list:
+    #     print(osl["semanticTags"][0])
+    #     print(osl['confidence'])
+
+    confidence_dict = dict()
+    final_suggestion_iri_set = set()
+    for suggestion in datasource_suggestion_list + ols_suggestion_list:
         suggested_term_iri = suggestion["semanticTags"][0]  # E.g. http://purl.obolibrary.org/obo/HP_0004839
-        suggested_term = create_local_term(suggested_term_iri)
+        confidence_dict.setdefault(suggested_term_iri, suggestion['confidence'])
+        if get_ontology_id(suggested_term_iri) in ["efo", "ordo", "hp", "mondo"]:
+            final_suggestion_iri_set.add(suggested_term_iri)
+
+    # print(confidence_dict)
+    print(final_suggestion_iri_set)
+    # A set of suggested terms found in the query and created in the app's database, used to exclude those terms from
+    # being deleted from the database when the delete_unused_mappings function is called.
+    created_terms = set()
+    for suggested_iri in final_suggestion_iri_set:
+        suggested_term = create_local_term(suggested_iri)
+        created_terms.add(suggested_term)
         create_mapping_suggestion(trait, suggested_term)
-        suggested_terms.add(suggested_term)
-    delete_unused_suggestions(trait, suggested_terms)
+    print("LENGTH")
+    print(len(created_terms))
+    delete_unused_suggestions(trait, created_terms)
+    find_automatic_mapping(trait, created_terms, confidence_dict)
 
 
-def get_zooma_suggestions_for_trait(trait):
+def get_suggestions_from_datasources(trait):
+    """
+    Takes in a trait object as its argument and returns the zooma response of a suggestion list as a dictionary.
+    """
+    formatted_trait_name = trait.name.replace(' ', '+')
+    print(f"{BASE_URL}/services/annotate?propertyValue={formatted_trait_name}"
+          "&filter=required:[cttv,sysmicro,atlas,ebisc,uniprot,gwas,cbi]")
+    response = requests.get(
+        f"{BASE_URL}/services/annotate?propertyValue={formatted_trait_name}"
+        "&filter=required:[cttv,sysmicro,atlas,ebisc,uniprot,gwas,cbi]")
+    return response.json()
+
+
+def get_suggestions_from_ols(trait):
     """
     Takes in a trait object as its argument and returns the zooma response of a suggestion list as a dictionary.
     """
     formatted_trait_name = trait.name.replace(' ', '+')
     response = requests.get(
-        f"{BASE_URL}/services/annotate?propertyValue={formatted_trait_name} \
-        &filter=required:[none],ontologies:[efo,mondo,hp,ordo]")
+        f"{BASE_URL}/services/annotate?propertyValue={formatted_trait_name}"
+        "&filter=required:[none],ontologies:[efo,mondo,hp,ordo]")
     return response.json()
 
 
@@ -114,16 +154,51 @@ def create_mapping_suggestion(trait, term, user_email='eva-dev@ebi.ac.uk'):
     logger.info(f"Created mapping suggestion {suggestion}")
 
 
+<<<<<<< HEAD
 @transaction.atomic
 def delete_unused_suggestions(trait, suggested_terms):
+=======
+def find_automatic_mapping(trait, created_terms, confidence_dict):
+    if trait.status != Status.UNMAPPED:
+        return
+
+    zooma_user = User.objects.filter(email='eva-dev@ebi.ac.uk').first()
+    for term in created_terms:
+        print('EY', confidence_dict.get(term.iri))
+        if confidence_dict.get(term.iri) == 'HIGH':
+            if Mapping.objects.filter(trait_id=trait, term_id=term).exists():
+                continue
+            mapping = Mapping(trait_id=trait, term_id=term,
+                              curator=zooma_user, is_reviewed=False)
+            mapping.save()
+            trait.current_mapping = mapping
+            trait.save()
+            print('CREATED CONF', trait.current_mapping)
+            return
+
+    for term in created_terms:
+        if trait.name.lower() == term.label.lower():
+            if Mapping.objects.filter(trait_id=trait, term_id=term).exists():
+                continue
+            mapping = Mapping(trait_id=trait, term_id=term,
+                              curator=zooma_user, is_reviewed=False)
+            mapping.save()
+            trait.current_mapping = mapping
+            trait.save()
+            print('CREATED TEXT MATCH', trait.current_mapping)
+            return
+
+
+def delete_unused_suggestions(trait, created_terms):
+>>>>>>> Create initial automatic mapping functionality
     """
-    Takes in a trait and a set of suggested_terms, found in the previously executed ZOOMA query. This function gets all
-    the mapping suggestions for that trait that are NOT found in the suggested_terms list or in any previous mappings
+    Takes in a trait and a set of created_terms, found in the previously executed ZOOMA query. This function gets all
+    the mapping suggestions for that trait that are NOT found in the created_terms list or in any previous mappings
     for that trait, and deletes them
     """
     trait_mappings = trait.mapping_set.all()
     for mapping in trait_mappings:
-        suggested_terms.add(mapping.term_id)
-    deleted_suggestions = trait.mappingsuggestion_set.exclude(term_id__in=list(suggested_terms))
+        created_terms.add(mapping.term_id)
+    deleted_suggestions = trait.mappingsuggestion_set.exclude(term_id__in=list(created_terms))
     deleted_suggestions.delete()
-    logger.info(f"Deleted mapping suggestions {deleted_suggestions}")
+    logger.info(f"Deleted mapping suggestions {deleted_suggestions.all()}")
